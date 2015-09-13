64dc62c8d6ab54124d59e93d198cff49400f96c6
shawn-dsz <shawndsouza@outlook.com>
2015-09-13 13:14:28 +1200

    init

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+using Sync.Core;
+using Sync.Helpers;
+using Sync.Models;
+
+namespace Sync
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Start(args);
+
+            Console.Write("Done");
+            Console.Read();
+        }
+
+        private static void Start(string[] args)
+        {
+            var fileHelper = new FileInfoHelper();
+
+            var dirs = fileHelper.GetDirectoryInfo(args).ToList();
+
+            var syncDirA = new SyncDirectory(dirs.ElementAt(0));
+            var syncDirB = new SyncDirectory(dirs.ElementAt(1));
+
+            var syncMan = new SyncManager();
+            syncMan.Sync(syncDirA, syncDirB);
+        }
+
+    }
+}
